402cfb2de8af0608a92c91192cd0d0e0304dd3e2
Jacob Hands <jacob@gogit.io>
2022-10-29 09:21:25 -0500

    It works!

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,9 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+// Import papaparse
+import Papa from "papaparse"
 
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
+type UUIDMessage = {
+	id_type: number
+	id: string
 }
 
 export default {
@@ -25,6 +12,49 @@ export default {
 		env: Env,
 		ctx: ExecutionContext
 	): Promise<Response> {
-		return new Response("Hello World!");
+		if (request.method === 'POST') {
+			const url = new URL(request.url)
+			const apiKey = url.searchParams.get('key')
+			if (apiKey !== env.API_KEY) {
+				return new Response('forbidden', { status: 403 })
+			}
+			const msg: UUIDMessage = await request.json()
+			await env.QUEUE.send(msg)
+		}
+		return new Response("Ok")
 	},
-};
+
+	// Invoked when the Worker receives a batch of messages.
+	async queue(batch: MessageBatch<UUIDMessage>, env: Env) {
+		// Extract the body from each message.
+		// Metadata is also available, such as a message id and timestamp.
+		const messages: UUIDMessage[] = batch.messages.map((msg) => msg.body)
+
+		await runTask(messages, env);
+	},
+}
+
+// Replace this function with your own code!
+//
+// You can run tasks and batch jobs, such as:
+// - Invoking a webhook
+// - Storing data in a R2 bucket
+// - Sending telemetry data to a provider.
+async function runTask(messages: UUIDMessage[], env: Env) {
+	console.log("Received a batch of", messages.length, "messages:", messages);
+	// convert to csv
+	const csv = Papa.unparse(messages)
+	console.log(csv)
+	// upload to r2
+	const timestamp = new Date().toISOString()
+		.replaceAll('-', '/')
+		.replaceAll(':', '-')
+		.replace('.', '-')
+		.replace('T', '/')
+	const filename = `uuids/${timestamp}.csv`
+	await env.UUIDS.put(filename, csv, { httpMetadata: { contentType: "text/csv" } })
+	// const parsedMessages = messages.map((message) => JSON.parse(message) as UUIDMessage)
+	// If the task fails, the batch will be retried.
+	// You can configure the max_retries in the `wrangler.toml`.
+	await new Promise((resolve) => setTimeout(resolve, 3000, []));
+}
---
+++ src/types.d.ts
@@ -0,0 +1,13 @@
+type Env = {
+  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+  // MY_KV_NAMESPACE: KVNamespace;
+  //
+  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
+  // MY_DURABLE_OBJECT: DurableObjectNamespace;
+  //
+  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
+  // MY_BUCKET: R2Bucket;
+  UUIDS: R2Bucket
+  QUEUE: Queue
+  API_KEY: string
+}
